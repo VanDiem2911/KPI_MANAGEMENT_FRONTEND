482192f87a6d64dffd7ab1e590678cdca7b30a10
VanDiem2911 <vandiem2004@gmail.com>
2026-07-08 14:49:16 +0700

    add api analyst

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Tracker from "kpi-tracker";
+
+
+Tracker.init({
+  apiKey: "kpi_0a60d682d45f4131a2a6c21fa5355eb6",
+  serverUrl: "https://api-analytics-backend.onrender.com" // Endpoint backend API
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
