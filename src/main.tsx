5ff9bb00ef0f9216f1cd537f826c323354df66d2
VanDiem2911 <vandiem2004@gmail.com>
2026-07-08 15:00:12 +0700

    update npm api

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Tracker from "kpi-tracker";
+import Tracker from "kpi-tracker-vandiem";
 
 
 Tracker.init({
